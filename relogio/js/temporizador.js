376d96b85feb3853726559dc93a66882aee1708a
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-03-27 10:46:39 -0300

    Finalizei o temporizador
    talvez tenha algum erro pequeno, mas eu não percebi nada

--- relogio/js/temporizador.js
+++ relogio/js/temporizador.js
@@ -11,66 +11,90 @@ var intervalo = function(){}
 let iniciou = false
 iniciar.addEventListener('click',() => {
     if(!iniciou){
-        console.log("entrou")
-        console.log(segundos)
         iniciou = !iniciou
-        let mil = parseInt(milesimos.innerHTML)
-        let s = parseInt(segundos.innerHTML)
-        let m = parseInt(minutos.innerHTML)
-        let h = parseInt(horas.innerHTML)
-        console.log(mil)
-        console.log(s)
-        if(s < 10){
+
+        let mil = milesimos.value
+        let s = segundos.value
+        let m = minutos.value
+        let h = horas.value
+
+        if(mil == ''){
+            mil = '00'
+        }
+        if(s == ''){
+            s = '00'
+        }
+        if(m == ''){
+            m = '00'
+        }
+        if(h == ''){
+            h = '00'
+        }
+        
+        if(s < 10 && s.length < 2){
             s = '0' + s
         }
-        if(m < 10){
+        if(m < 10 && m.length < 2){
             m = '0' + m
         }
-        if(h < 10){
+        if(h < 10 && h.length < 2){
             h = '0' + h
         }
-
+        
         intervalo = setInterval(() => {
-            mil = parseInt(milesimos.innerHTML)
+            mil = milesimos.value
             mil -= 5
 
             if(mil <= 0){
-                s--
-                mil = '99'
-                if(s < 10){
-                    s = '0' + s
+                if(s >= 1){
+                    mil = '99'
+                    s--
+                    if(s < 10){
+                        s = '0' + s
+                    }
+                }
+
+                if(s == 0){
+                    if(m >= 1){
+                        s = 60
+                        m--
+                        if(m < 10){
+                            m = '0' + m
+                        }
+                    }
+
+                    if(m == 0){
+                        if(h >= 1){
+                            m = 60
+                            h--
+                            if(h < 10){
+                                h = '0' + h
+                            }
+                        }
+                    }
                 }
             }
 
-            if(s == 0){
-                s = 60
-                m--
-                if(s < 10){
-                    s = '0' + s
-                }
-                if(m < 10){
-                    m = '0' + m
-                }
+            milesimos.value = mil
+            segundos.value = s
+            minutos.value = m
+            horas.value = h
+
+            if(s <= 00 && mil <= 0){
+                clearInterval(intervalo)
+                milesimos.value = '00'
+                iniciou = false
             }
-            
-            if(m == 0){
-                m = 60
-                h--
-                if(m < 10){
-                    m = '0' + m
-                }
-                if(h < 10){
-                    h = '0' + h
-                }
-            }
-            
-            milesimos.innerHTML = mil
-            segundos.innerHTML = s
-            minutos.innerHTML = m
-            horas.innerHTML = h
 
         },50)
-
-
     }
 })
+
+pausar.addEventListener('click',() => {
+    if (iniciou) {
+        clearInterval(intervalo);
+        iniciou = false;
+    }
+})
+
+
